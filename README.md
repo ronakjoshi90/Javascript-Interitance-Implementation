0a1a0bacfb5c493ad56f05925d8a20bbb8f9b452
ronakjoshi90 <ronak.joshi90@gmail.com>
2021-07-30 19:44:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-swenrz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-swenrz)
---
+++ index.js
@@ -0,0 +1,37 @@
+function Person(name, age) {
+  this.age = age;
+  this.name = name;
+}
+
+Person.prototype.getAge = function() {
+  console.log('age for ' + this.name + ' is ' + this.age);
+  return this.age;
+};
+
+Person.prototype.getName = function() {
+  console.log('name of person is ' + this.name);
+  return this.name;
+};
+
+function Employee(name, age, department) {
+  Person.call(this, name, age);
+  this.department = department;
+}
+
+Employee.prototype.getDepartmen = function() {
+  console.log('dept of person is ' + this.department);
+  return this.department;
+};
+
+Employee.prototype = Object.create(Person.prototype);
+
+var ronak = new Person('Tom', 31);
+var res = ronak.getAge();
+
+var personObj = new Employee('Ronak Joshi', 30, 'IT');
+var personObj2 = new Employee('Sonam', 25, 'Accounts');
+
+console.log(personObj);
+
+personObj.getAge();
+personObj2.getAge();
